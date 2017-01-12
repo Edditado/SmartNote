c084ced99944d2edcc0ee242a271583f58eb8414
Eddie Hurtado <edditado.zx@gmail.com>
2017-01-12 01:11:23 -0500

    Cambios en disenos de listas, se anadio boton de menu a cada elemento de la lista en Ciclos. Correccion en boton de creacion de Ciclos y Materias

--- SmartNote/platforms/browser/www/js/app.js
+++ SmartNote/platforms/browser/www/js/app.js
@@ -1,62 +1,65 @@
 var myApp = new Framework7({
-    material: true,
-    swipeout: false,
-    modalTitle: false,
+  material: true,
+  swipeout: false,
+  modalTitle: false,
 });
 
 var $$ = Dom7;
 
-
-
-$$('a').on('click', function (e) { //Close panel when you open a new page
-    myApp.closePanel();
-});
-
-
 var mainView = myApp.addView('.view-main');
 
-$$('#new_ciclo').on('click', function () {
-   myApp.modal({
-  
-  text: '<p style="color:black;font-weight:bold; font-size: 115%" >Nuevo Ciclo</p>',
-  afterText: '<input type="text" id="nuevoCiclo" class="modal-text-input" Placeholder="Nombre" autofocus>',
-  buttons: [{
-      text: 'CANCELAR',
-      onClick: function() {
-      
-    }
-  }, {
-    text: 'CREAR',
-    onClick: function() {
-      var nombCiclo=$$('#nuevoCiclo').val();
-      
-    }
-  }, ]
-});
+$$('a').on('click', function (e) { //Close panel when you open a new page
+  myApp.closePanel();
 });
 
 
+myApp.onPageInit('ciclos', function (page) {
 
-$$('#new_materia').on('click', function () {
-   myApp.modal({
-  
-  text: '<p style="color:black;font-weight:bold; font-size: 115%" >Nueva Materia</p>',
-  afterText: '<input type="text" id="nuevaMat" class="modal-text-input" Placeholder="Nombre" autofocus>',
-  buttons: [{
-      text: 'CANCELAR',
-      onClick: function() {
-       
-    }
-  }, {
-    text: 'CREAR',
-    onClick: function() {
-      var nombCiclo=$$('#nuevaMat').val();
-      
-    }
-  }, ]
+  $$('#new_ciclo').on('click', function () {   
+    myApp.modal({
+      text: '<p style="color:black;font-weight:bold; font-size: 115%" >Nuevo Ciclo</p>',
+      afterText: '<input type="text" id="nuevoCiclo" class="modal-text-input" placeholder="Nombre" autofocus>',
+      buttons: [
+        {
+          text: 'CANCELAR',
+          onClick: function(){
+          }
+        }, 
+        {
+          text: 'CREAR',
+          onClick: function() {
+            var nombCiclo = $$('#nuevoCiclo').val();
+          }
+        }, 
+      ]
+    });
+  });
+
 });
+
+
+myApp.onPageInit('materias', function (page) {
+
+  $$('#new_materia').on('click', function () {
+    myApp.modal({
+      text: '<p style="color:black;font-weight:bold; font-size: 115%" >Nueva Materia</p>',
+      afterText: '<input type="text" id="nuevaMat" class="modal-text-input" placeholder="Nombre" autofocus>',
+      buttons: [
+        {
+          text: 'CANCELAR',
+          onClick: function() { 
+          }
+        }, 
+        {
+          text: 'CREAR',
+          onClick: function() {
+            var nombCiclo=$$('#nuevaMat').val();
+          }
+        }, 
+      ]
+    });
+  });
+
 });
-
-
 
 
